2a89623e941a96cf63d11036e9d0c865653c3f6d
avi9360 <savinash406@gmail.com>
2021-05-20 12:40:11 +0530

    Contact

---
+++ src/components/ContactList.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function ContactList() {
+  return (
+    <div>
+      <div className="ui fixed menu">
+        <div className="ui container center">
+          <h2>Conatact Manager</h2>
+        </div>
+      </div>
+    </div>
+  );
+}
